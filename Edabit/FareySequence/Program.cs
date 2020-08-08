6a6e54d65acf81da0321dfd204706aae1bae23cb
unknown <gyetvaid@gmail.com>
2020-08-08 11:42:00 +0200

    Uploading solution

---
+++ Edabit/FareySequence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace FareySequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Farey(4);
+            foreach(var item in res)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        public static string[] Farey(int n)
+	{
+			var sortedElements = new SortedList();
+			string fraction;
+			for(int i = 0; i <= n; i++)
+			{
+				for(int j = i; j <= n; j++)
+				{
+					if(j == 0) continue;
+					fraction = i + "/" + j;
+					if(!sortedElements.ContainsKey(i / (double) j))
+					{
+						sortedElements.Add(i / (double) j, fraction);
+					}
+				}
+			}
+			var values = sortedElements.GetValueList();
+			var result = new string[sortedElements.Count];
+			for(int i = 0; i < result.Length; i++)
+			{
+				result[i] = (string) values[i];
+			}
+			return result;
+		}
+    }
+}
