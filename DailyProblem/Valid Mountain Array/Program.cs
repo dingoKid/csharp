241a91753cce7ca6627ab837272949de2c378985
Denes Gyetvai <35926364+dingoKid@users.noreply.github.com>
2020-08-08 10:45:01 +0200

    Add files via upload

---
+++ DailyProblem/Valid Mountain Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Valid_Mountain_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidMountainArray(new int[]{-1, -2, -2, -1}));
+        }
+
+        static bool ValidMountainArray(int[] heights)
+        {
+            if(heights.Length == 0 || heights is null) return false;
+            var firstNumber = heights.First();
+            var result = heights.SkipWhile(x => x <= firstNumber);
+            return result.Any(x => x < result.First());
+        }
+    }
+}
