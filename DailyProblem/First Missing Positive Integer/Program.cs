99779c5ae9c5d7346af3636af215f0dc0f072322
Denes Gyetvai <35926364+dingoKid@users.noreply.github.com>
2020-09-03 15:09:00 +0200

    Add files via upload
    Uploading solution

---
+++ DailyProblem/First Missing Positive Integer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace First_Missing_Positive_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MissingInteger(new int[]{3, 4, -1, 1, 2}));
+        }
+
+        static int MissingInteger(int[] numbers)
+        {
+            var positivePartOfList = new List<int>(numbers).Where(x => x > 0).OrderBy(x => x);
+            int result = 1;
+            while(true)
+            {
+                if(!positivePartOfList.Contains(result)) break;
+                result++;
+            }
+            return result;
+        }
+    }
+}
