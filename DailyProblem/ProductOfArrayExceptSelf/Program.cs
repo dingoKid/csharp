edcaa59fd83ffb684a7e485af23e87f43acb17ba
Denes Gyetvai <35926364+dingoKid@users.noreply.github.com>
2020-09-03 14:35:59 +0200

    Add files via upload
    Uploading solution

---
+++ DailyProblem/ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] val = Products(new int[]{1, 2, 3, 4, 5});
+            foreach(var item in val)
+             {
+                 System.Console.WriteLine(item);
+             }
+        }
+
+        static int[] Products(int[] array)
+        {
+            int[] result = new int[array.Length];
+            for(int i = 0; i < array.Length; i++)
+            {
+                int product = 1;
+                for(int j = 0; j < array.Length; j++)
+                {
+                    if(j != i) product *= array[j];
+                }
+                result[i] = product;
+            }
+            return result;
+        }
+    }
+}
