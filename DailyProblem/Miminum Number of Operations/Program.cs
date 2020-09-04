516378a1aeb4611e07ffc23ddae7f721c4bfd03a
Denes Gyetvai <35926364+dingoKid@users.noreply.github.com>
2020-09-04 07:46:43 +0200

    Add files via upload
    Uploading solution

---
+++ DailyProblem/Miminum Number of Operations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Miminum_Number_of_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumOperations(6, 19));
+        }
+
+        static int MinimumOperations(int x, int y)
+        {
+            int operations = 0;
+            if(y < x) return x-y;
+            if(y % 2 == 1) 
+            {
+                operations++;
+                y++;
+            }
+
+            while(x <= y)
+            {
+                if(x == y) return operations;
+                y /= 2;
+                operations++;
+            }
+            operations = operations + x - y;
+            return operations;
+        }
+    }
+}
